ccf18a617590463fbec6cddf97dcc87e7ecfb9fc
UdayaPriyaS01 <43386341+UdayaPriyaS01@users.noreply.github.com>
2021-12-10 11:50:10 -0500

    Project1

---
+++ BasicEndlessRunner/Assets/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+    int score;
+    static int lives = 3;
+    public static GameManager inst;
+
+    [SerializeField] Text scoreText;
+    [SerializeField] Text LivesText;
+    [SerializeField] Text GameoverText;
+
+    [SerializeField] PlayerMovement playerMovement;
+
+    public void IncrementScore ()
+    {
+        score++;
+        scoreText.text = "SCORE: " + score;
+        // Increase the player's speed
+
+        if (score == 25)
+        {
+            DisplayGameover();
+
+        }
+
+        playerMovement.speed += playerMovement.speedIncreasePerPoint;
+    }
+
+    public int DecrementLives()
+    {
+        lives--;
+        LivesText.text = "LIVES LEFT: " + lives;
+
+        return lives;
+    }
+
+    public void DisplayGameover()
+    {
+        if (lives > 0)
+        {
+             GameoverText.text = "YOU WIN!!";
+        }
+        else
+        {
+            GameoverText.text = "GAME OVER!!";
+        }
+        
+    }
+
+    private void Awake ()
+    {
+        inst = this;
+    }
+
+    private void Start () {
+
+	}
+
+	private void Update () {
+
+    }
+}
